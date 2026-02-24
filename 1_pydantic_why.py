83ea0e737dcf75d6b5a21e901c6f2cb17e850241
shreyass0007 <shreshshende.777@gmail.com>
2026-02-24 20:48:27 +0530

    optional module

--- 1_pydantic_why.py
+++ 1_pydantic_why.py
@@ -1,20 +1,37 @@
 from pydantic import BaseModel
+from typing import List , Dict, Optional
 
 class Patient(BaseModel):
     name:str
     age:int
     weight:float
+    married:bool
+    allergies:Optional[list[str]]=None
+    contact_info:dict[str,str] 
 
 def insert_patient_data(patient:Patient):
     if type(patient.name)==str and type(patient.age)==int:
         print(patient.name)
         print(patient.age)
+        print(patient.weight)
+        print(patient.married)
+        print(patient.allergies)
+        print(patient.contact_info)
         print('inserted into database')
     else:
         raise TypeError('Invalid data types')
 
+def updated_patient_data(patient:Patient):
+    print(patient.name)
+    print(patient.age)
+    print(patient.weight)
+    print(patient.married)
+    print(patient.allergies)
+    print(patient.contact_info)
+    print('Updated patient data in database')
 
-patient_info={'name':'shreyas','age':35}
+patient_info={'name':'shreyas','age':35, 'weight':70.5, 'married':True, 'contact_info':{'phone':'1234567890','email':'shreyas@example.com'}}  
 
 patient1=Patient(**patient_info)
-insert_patient_data(patient1)
+# insert_patient_data(patient1)
+updated_patient_data(patient1)
