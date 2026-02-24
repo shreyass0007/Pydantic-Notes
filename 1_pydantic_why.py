7098d4cc93e36f7146821e28fcfabdb0220e4ea7
shreyass0007 <shreshshende.777@gmail.com>
2026-02-24 21:24:15 +0530

    Using datatype EmailStrr , AnyUrl

--- 1_pydantic_why.py
+++ 1_pydantic_why.py
@@ -1,10 +1,12 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr , AnyUrl , Field
 from typing import List , Dict, Optional
 
 class Patient(BaseModel):
-    name:str
+    name:str=Field(max_length=50)
     age:int
-    weight:float
+    email:EmailStr='abc@gmail.com'
+    linkedin_url:Optional[AnyUrl]=None
+    weight:float=Field(gt=0)
     married:bool
     allergies:Optional[list[str]]=None
     contact_info:dict[str,str] 
@@ -13,6 +15,7 @@ def insert_patient_data(patient:Patient):
     if type(patient.name)==str and type(patient.age)==int:
         print(patient.name)
         print(patient.age)
+        print(patient.email)
         print(patient.weight)
         print(patient.married)
         print(patient.allergies)
@@ -24,13 +27,15 @@ def insert_patient_data(patient:Patient):
 def updated_patient_data(patient:Patient):
     print(patient.name)
     print(patient.age)
+    print(patient.email)
+    print(patient.linkedin_url)
     print(patient.weight)
     print(patient.married)
     print(patient.allergies)
     print(patient.contact_info)
     print('Updated patient data in database')
 
-patient_info={'name':'shreyas','age':35, 'weight':70.5, 'married':True, 'contact_info':{'phone':'1234567890','email':'shreyas@example.com'}}  
+patient_info={'name':'shreyas','age':35,'email':'shreyas@example.com','linkedin_url':'https://www.linkedin.com/in/shreyas','weight':70.5, 'married':True, 'contact_info':{'phone':'1234567890','email':'shreyas@example.com'}}  
 
 patient1=Patient(**patient_info)
 # insert_patient_data(patient1)
