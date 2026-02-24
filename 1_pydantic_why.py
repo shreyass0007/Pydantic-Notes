d20a63ed7cea8e8f589e91103573d0c358cbc3df
shreyass0007 <shreshshende.777@gmail.com>
2026-02-24 20:21:51 +0530

    BaseModel

--- 1_pydantic_why.py
+++ 1_pydantic_why.py
@@ -1,6 +1,20 @@
-def insert_patient_data(name: str,age: int):
-    print(name)
-    print(age)
-    print('inserted into database')
+from pydantic import BaseModel
 
-insert_patient_data('John','30')
+class Patient(BaseModel):
+    name:str
+    age:int
+    weight:float
+
+def insert_patient_data(patient:Patient):
+    if type(patient.name)==str and type(patient.age)==int:
+        print(patient.name)
+        print(patient.age)
+        print('inserted into database')
+    else:
+        raise TypeError('Invalid data types')
+
+
+patient_info={'name':'shreyas','age':35}
+
+patient1=Patient(**patient_info)
+insert_patient_data(patient1)
